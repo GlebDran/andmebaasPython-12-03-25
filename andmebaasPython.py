ea31d454ca8291b6a2f5526e6bfe0572d296ae26
opilane <opilane@HP-CZC2349HT0>
2025-03-12 14:54:43 +0200

    updated

--- andmebaasPython.py
+++ andmebaasPython.py
@@ -1,1 +1,14 @@
+from sqlite3 import *
+from sqlite3 import Error
+
+def create_connectrion (path):
+    connection = None
+    try:
+        connection = connect(path)
+        print("Ühendus on edukalt tehtud")
+    except Error as e:
+        print(f"Tekkis vig'{e}'")
+    return connection
+conn=create_connection("C:\Users\opilane\source\repos\andmebaasPython\AppData\data.db")
+
 
